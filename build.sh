fe9e7697fc31c04a7d15d4a6d216a18d6d1d172e
Andrew Chen <andrew@jaanuu.com>
2025-12-25 07:40:26 -0800

    Refactor build.sh to remove random tag generation and add GitHub Actions workflow for building and pushing Docker images

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-TAG=$(date -u '+%Y%m%d%H%M%S')-$(openssl rand -base64 4 | tr -d "=+/") docker buildx bake -f docker-bake.hcl --push
+TAG=$(date -u '+%Y%m%d%H%M%S') docker buildx bake -f docker-bake.hcl --push
